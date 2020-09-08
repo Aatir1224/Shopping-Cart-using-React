de03df2cc5f87daaa3c1d56ecc70ed2d6a3e90ff
Aatir <aatirqureshi1@gmail.com>
2020-09-08 11:07:12 +0530

    added cart functionality

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+
+function App() {
+  const [cartItem, setCartItem] = useState([]);
+
+  const addInCart = item => {
+    const isAlreadyAdded = cartItem.findIndex(function(array){
+      return array.id === item.id
+    })
+    if(isAlreadyAdded !== -1){
+      toast("Already Added in Cart", {
+        type:"error"
+      })
+    }
+    setCartItem(...cartItem,item)
+  }
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
