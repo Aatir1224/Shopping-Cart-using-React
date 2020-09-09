09e125c7e35395df18f10c2249dcf0e21d3283cb
Aatir <aatirqureshi1@gmail.com>
2020-09-09 12:56:48 +0530

    completed the shopping cart using React

--- src/components/BuyPage.js
+++ src/components/BuyPage.js
@@ -1,60 +1,56 @@
-import React, {useState, useEffect} from 'react';
-import Axios from 'axios';
-import {random, commerce} from 'faker';
-import {Container, Row, Col} from 'reactstrap';
-import CartItem from './CartItem';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import CartItem from "./CartItem";
 
-// const apiKey = "INSERT_YOUR_KEY_HERE";
-// const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1"
-// const localURL = "https://www.jsonware.com/json/810832bc3acc8fae3fb8ea04df64ed34.json"
+import { random, commerce } from "faker";
+import { Container, Col, Row } from "reactstrap";
 
-const BuyPage = ({addInCart}) => {
-    const [product, setProduct] = useState([]);
+const apiKey = "INSET_YOUR_KEY_HERE";
 
-    // const fetchPhotos = async () => {
-    //     const response = await Axios.get('url',{
-    //         header:{
-    //             Authorization:apiKey
-    //         }
-    //     });
+const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
+const localurl = "https://www.jsonware.com/json/810832bc3acc8fae3fb8ea04df64ed34.json";
+const BuyPage = ({ addInCart }) => {
+  const [product, setProduct] = useState([]);
 
-    const fetchPhotos = async () => {
-        const {data} = await Axios.get("https://www.jsonware.com/json/810832bc3acc8fae3fb8ea04df64ed34.json",{});
-        console.log(data);
-        const {photos} = data;
-        console.log(photos)
+  //   const fetchPhotos = async () => {
+  //     const response = await Axios.get(url, {
+  //       header: {
+  //         Authorization: apiKey
+  //       }
+  //     });
 
-        
-    const allProducts = photos.map(photo => ({
-        smallImage: photo.src.medium,
-        tinyImage: photo.src.tiny,
-        productName: random.word(),
-        productPrice: commerce.price(),
-        id:random.uuid() 
+  const fetchPhotos = async () => {
+    const { data } = await Axios.get(localurl, {});
+
+    const { photos } = data;
+
+    const allProduct = photos.map(photo => ({
+      smallImage: photo.src.medium,
+      tinyImage: photo.src.tiny,
+      productName: random.word(),
+      productPrice: commerce.price(),
+      id: random.uuid()
     }));
 
-    setProduct(allProducts)
+    setProduct(allProduct);
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  return (
+    <Container fluid>
+      <h1 className="text-success text-center">Buy Page</h1>
+      <Row>
+        {product.map(product => (
+          <Col md={4} key={product.id}>
+            <CartItem product={product} addInCart={addInCart} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 };
-
-    useEffect(() => {
-        fetchPhotos()
-    }, []);
-
-    return(
-        <Container fluid>
-            <h1 className="text-success text-center">
-                Buy Page
-            </h1>
-            <Row>
-                {product.map(product => (
-                    // <span key={product.id}>{product.productName}</span>
-                    <Col md={4} key={product.id}>
-                        <CartItem product={product} />
-                    </Col>
-                ))}
-            </Row>
-        </Container>
-    )
-}
 
 export default BuyPage;
